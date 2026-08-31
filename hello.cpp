71edccc5d3a67da7ac60d742701c69b3458f0664
DGL20102028 <derek1049961@my.yosemite.edu>
2026-08-31 11:38:19 -0700

    Update header info

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 // CSCI 27X - GitHub Upload Practice
-// Name: YOUR NAME
-// Section: YOUR SECTION
+// Name: Derek Garcia-Leon
+// Section: MCSCI-271-1285
 
 #include <iostream>
 using namespace std;
